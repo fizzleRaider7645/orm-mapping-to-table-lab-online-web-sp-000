36ec9d55c0b2a65874b7b940034606082fffc323
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 16:00:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,7 @@ class Student
   
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) VALUES ()
+      INSERT INTO students (name, grade) VALUES (self.name, self.grade)
     SQL
   end
   

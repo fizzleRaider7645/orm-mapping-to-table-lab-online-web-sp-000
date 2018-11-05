2e98e10c23e4d9af6b9723e7f259a86f75688b8f
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 15:55:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,8 +11,15 @@ class Student
     @id = id
   end
   
-   def self.create_table
-     p DB[:conn]
+    def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
   end
   
 end

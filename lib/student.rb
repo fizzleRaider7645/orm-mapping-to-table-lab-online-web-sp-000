2ec9398a8ae8b829384aa7feb0eb74697216944d
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-05 16:12:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
       VALUES (?, ?);
       SQL
     DB[:conn].execute(sql, self.name, self.grade)
-    @id =  DB[:conn].execute
+    p DB[:conn].execute("SELECT * FROM students ORDER BY column DESC LIMIT 1;")
   end
   
 end
